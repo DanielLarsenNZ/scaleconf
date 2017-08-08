a0c247e0e5b5ccf05f6e3e81418b58788b3a66de
Daniel Larsen <dalars@microsoft.com>
2017-08-09 07:56:46 +1200

    + API Project

---
+++ src/Sconf/Sconf.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Sconf.Api.Controllers
+{
+    public class HealthController : ApiController
+    {
+        // GET: api/Health
+        public dynamic Get()
+        {
+            return new { Status = "Ok", Version = "1.0.0", Now = DateTime.UtcNow };
+        }
+    }
+}
---
+++ src/Sconf/Sconf.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sconf.Api.WebApiApplication" Language="C#" %>
